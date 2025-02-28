b6edc48d304a57ff49241d2e462ff29e8a01f14b
Md. Mazharul Islam <mazharul.saurav255@gmail.com>
2025-02-28 17:52:04 +0600

    API-Permission
    API-Permission

--- routes/api.js
+++ routes/api.js
@@ -17,31 +17,31 @@ router.post('/Registration', AuthController.Registration);
 router.post('/VerifyLogin',AuthController.VerifyLogin);
 router.post('/AuthDestroy',AuthController.AuthDestroy);
 
-router.post('/userdetails',AuthVarification,Userdetails.userdetails);
-router.post('/sendmail',AuthVarification,Userdetails.sendmail)
+router.post('/userdetails',AuthVarification,checkPermission('userdetails'),Userdetails.userdetails);
+router.post('/sendmail',AuthVarification,checkPermission('sendmail'),Userdetails.sendmail)
 
 
-router.post('/category',AuthVarification,ProductController.category)
-router.post('/brands',AuthVarification,checkPermission('can_delete'),ProductController.brands)
-router.post('/units',AuthVarification,ProductController.units)
-router.post('/product',AuthVarification,ProductController.product)
-router.post('/stock',AuthVarification,ProductController.stock)
-router.post('/purchasesuppliertracker',AuthVarification,ProductController.purchasesuppliertracker)
-router.get('/supplierpurchaseinvoice',AuthVarification,ProductController.supplierpurchaseinvoice)
+router.post('/category',AuthVarification,checkPermission('createCategory'),ProductController.category)
+router.post('/brands',AuthVarification,checkPermission('createBrands'),ProductController.brands)
+router.post('/units',AuthVarification,checkPermission('createUnits'),ProductController.units)
+router.post('/product',AuthVarification,checkPermission('createProducts'),ProductController.product)
+router.post('/stock',AuthVarification,checkPermission('viewStock'),ProductController.stock)
+router.post('/purchasesuppliertracker',AuthVarification,checkPermission('purchaseSupplierTracker'),ProductController.purchasesuppliertracker)
+router.get('/supplierpurchaseinvoice',AuthVarification,checkPermission('supplierPurchaseInvoice'),ProductController.supplierpurchaseinvoice)
 
-router.post('/supplier',AuthVarification,ProductController.supplier)
-router.get('/getpurchasesuppliertracker/:supplierid',AuthVarification,ProductController.getpurchasesuppliertracker)
+router.post('/supplier',AuthVarification,checkPermission('createSupplier'),ProductController.supplier)
+router.get('/getpurchasesuppliertracker/:supplierid',AuthVarification,checkPermission('getPurchaseSupplierTracker'),ProductController.getpurchasesuppliertracker)
 
-router.get('/dropdown/:type',AuthVarification,DropdownController.dropdown)
+router.get('/dropdown/:type',AuthVarification,checkPermission('dropdown'),DropdownController.dropdown)
 
-router.post('/customertype',AuthVarification,CustomerController.customertype)
-router.post('/customer',AuthVarification,CustomerController.customer)
-router.get('/customerledgerinvoice',AuthVarification,CustomerController.customerledgerinvoice)
+router.post('/customertype',AuthVarification,checkPermission('customerType'),CustomerController.customertype)
+router.post('/customer',AuthVarification,checkPermission('createCustommer'),CustomerController.customer)
+router.get('/customerledgerinvoice',AuthVarification,checkPermission('customerLedgerInvoice'),CustomerController.customerledgerinvoice)
 
-router.post('/saleproduct',AuthVarification,SaleController.saleproduct)
-router.post('/salesCustomerTracker',AuthVarification,SaleController.salesCustomerTracker)
-router.get('/getsalescustomertracker/:customerid',AuthVarification,SaleController.getsalescustomertracker)
-router.get('/salesinvoice',AuthVarification,SaleController.salesinvoice)
+router.post('/saleproduct',AuthVarification,checkPermission('saleProduct'),SaleController.saleproduct)
+router.post('/salesCustomerTracker',AuthVarification,checkPermission('salesCustomerTracker'),SaleController.salesCustomerTracker)
+router.get('/getsalescustomertracker/:customerid',AuthVarification,checkPermission('getSalesCustomerTracker'),SaleController.getsalescustomertracker)
+router.get('/salesinvoice',AuthVarification,checkPermission('salesInvoice'),SaleController.salesinvoice)
 
 
 
